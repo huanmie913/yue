96a5396f89643b09fe1355773155f216290aae7e
f2er <uezheng@gmail.com>
2012-11-27 07:56:11 +0800

    update

--- static/js/articleData.js
+++ static/js/articleData.js
@@ -15,7 +15,7 @@ var articleDataArray = [
 		  "title" : "攻城师的沟通修炼",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201210/1001.html"
+				"ajaxSource" : "content/201210/1001.html"
 		  }
 		}
 	},
@@ -30,7 +30,7 @@ var articleDataArray = [
 		  "title" : "模块化——高效重构",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201209/902.html"
+				"ajaxSource" : "content/201209/902.html"
 		  }
 		}
 	},
@@ -45,7 +45,7 @@ var articleDataArray = [
 		  "title" : "从技术到管理：思维转变是关键",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201209/901.html"
+				"ajaxSource" : "content/201209/901.html"
 		  }
 		}
 	},
@@ -60,7 +60,7 @@ var articleDataArray = [
 		  "title" : "动手好奇反叛的Hacker精神",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/811.html"
+				"ajaxSource" : "content/201208/811.html"
 		  }
 		}
 	},
@@ -75,7 +75,7 @@ var articleDataArray = [
 		  "title" : "事业",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/810.html"
+				"ajaxSource" : "content/201208/810.html"
 		  }
 		}
 	},
@@ -90,7 +90,7 @@ var articleDataArray = [
 		  "title" : "谈谈学习与沟通",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/809.html"
+				"ajaxSource" : "content/201208/809.html"
 		  }
 		}
 	},
@@ -105,7 +105,7 @@ var articleDataArray = [
 		  "title" : "【转】怎样成长为一个优秀的 Web 前端开发工程师？",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/808.html"
+				"ajaxSource" : "content/201208/808.html"
 		  }
 		}
 	},
@@ -120,7 +120,7 @@ var articleDataArray = [
 		  "title" : "【转】你的问题不是缺少动力",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/807.html"
+				"ajaxSource" : "content/201208/807.html"
 		  }
 		}
 	},
@@ -135,7 +135,7 @@ var articleDataArray = [
 		  "title" : "给IT新人的15个建议：苦逼程序员的辛酸反省与总结",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/806.html"
+				"ajaxSource" : "content/201208/806.html"
 		  }
 		}
 	},
@@ -150,7 +150,7 @@ var articleDataArray = [
 		  "title" : "【转】尊严的来处",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/805.html"
+				"ajaxSource" : "content/201208/805.html"
 		  }
 		}
 	},
@@ -165,7 +165,7 @@ var articleDataArray = [
 		  "title" : "降级论",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/804.html"
+				"ajaxSource" : "content/201208/804.html"
 		  }
 		}
 	},
@@ -180,7 +180,7 @@ var articleDataArray = [
 		  "title" : "InfoQ的ArchSummit大会对我的采访",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/803.html"
+				"ajaxSource" : "content/201208/803.html"
 		  }
 		}
 	},
@@ -195,7 +195,7 @@ var articleDataArray = [
 		  "title" : "日常工作与新技术",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/802.html"
+				"ajaxSource" : "content/201208/802.html"
 		  }
 		}
 	},
@@ -210,7 +210,7 @@ var articleDataArray = [
 		  "title" : "前端开发--十日谈",
 		  "ajaxcontent" : {
 				"ajaxFlag" : true,
-				"ajaxSource" : "/201208/801.html"
+				"ajaxSource" : "content/201208/801.html"
 		  }
 		}
 	}
