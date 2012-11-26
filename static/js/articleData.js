be67974aad6e58994fc133d70e0d978ec837d698
f2er <uezheng@gmail.com>
2012-11-27 07:49:24 +0800

    update

---
+++ static/js/articleData.js
@@ -0,0 +1,217 @@
+﻿/*
+ * to : 文章
+ * author : 情封
+ * time   : 2012-08-13 
+*/
+var articleDataArray = [
+	{
+		"id":1001,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://tech.weibo.com/archives/2103",
+		  "time" : "2012-10-16",
+		  "author" : "于海涛",
+		  "avatar" : "/static/images/avatar.png",
+		  "title" : "攻城师的沟通修炼",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201210/1001.html"
+		  }
+		}
+	},
+	{
+		"id":902,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://udc.weibo.com/2012/06/%E6%A8%A1%E5%9D%97%E5%8C%96%E2%80%94%E2%80%94%E9%AB%98%E6%95%88%E9%87%8D%E6%9E%84/",
+		  "time" : "2012-09-18",
+		  "author" : "燃烧的拖把",
+		  "avatar" : "http://tp1.sinaimg.cn/1747132320/50/5604906446/1",
+		  "title" : "模块化——高效重构",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201209/902.html"
+		  }
+		}
+	},
+	{
+		"id":901,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://www.programmer.com.cn/13382/",
+		  "time" : "2012-09-07",
+		  "author" : "梁景波",
+		  "avatar" : "/static/images/avatar.png",
+		  "title" : "从技术到管理：思维转变是关键",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201209/901.html"
+		  }
+		}
+	},
+	{
+		"id":811,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://home.wangjianshuo.com/cn/20120822_chackercc.htm",
+		  "time" : "2012-08-25",
+		  "author" : "王建硕",
+		  "avatar" : "http://tp3.sinaimg.cn/1682912122/50/1279886810/1",
+		  "title" : "动手好奇反叛的Hacker精神",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/811.html"
+		  }
+		}
+	},
+	{
+		"id":810,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://blog.sina.com.cn/s/blog_593af2a701016xme.html",
+		  "time" : "2012-08-23",
+		  "author" : "梁斌",
+		  "avatar" : "http://tp4.sinaimg.cn/1497035431/50/5608201828/1",
+		  "title" : "事业",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/810.html"
+		  }
+		}
+	},
+	{
+		"id":809,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://hi.baidu.com/caoz/blog/item/2fc87bcb0ea8a301bf09e66b.html",
+		  "time" : "2012-08-21",
+		  "author" : "caoz",
+		  "avatar" : "http://tp4.sinaimg.cn/1495169251/50/5630936243/1",
+		  "title" : "谈谈学习与沟通",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/809.html"
+		  }
+		}
+	},
+	{
+		"id":808,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://www.ux265.net/",
+		  "time" : "2012-08-17",
+		  "author" : "无",
+		  "avatar" : "http://tp2.sinaimg.cn/1067424897/50/5608307979/1",
+		  "title" : "【转】怎样成长为一个优秀的 Web 前端开发工程师？",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/808.html"
+		  }
+		}
+	},
+	{
+		"id":807,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://www.ux265.net/",
+		  "time" : "2012-08-17",
+		  "author" : "褪墨",
+		  "avatar" : "/static/images/avatar.png",
+		  "title" : "【转】你的问题不是缺少动力",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/807.html"
+		  }
+		}
+	},
+	{
+		"id":806,
+		"content" : {
+		  "from" : "",
+		  "url" : "http://www.ux265.net/",
+		  "time" : "2012-08-17",
+		  "author" : "无",
+		  "avatar" : "/static/images/avatar.png",
+		  "title" : "给IT新人的15个建议：苦逼程序员的辛酸反省与总结",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/806.html"
+		  }
+		}
+	},
+	{
+		"id":805,
+		"content" : {
+		  "from" : "http://coolshell.cn/articles/8031.html",
+		  "url" : "http://www.huxiu.com/portal.php?mod=view&aid=1344",
+		  "time" : "2012-08-17",
+		  "author" : "脱不花妹妹",
+		  "avatar" : "/static/images/avatar.png",
+		  "title" : "【转】尊严的来处",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/805.html"
+		  }
+		}
+	},
+	{
+		"id":804,
+		"content" : {
+		  "from" : "http://coolshell.cn/articles/8031.html",
+		  "url" : "http://meditic.com/degrading-for-success/",
+		  "time" : "2012-08-17",
+		  "author" : "meditic",
+		  "avatar" : "/static/images/avatar.png",
+		  "title" : "降级论",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/804.html"
+		  }
+		}
+	},
+	{
+		"id":803,
+		"content" : {
+		  "from" : "http://coolshell.cn/articles/8031.html",
+		  "url" : "http://coolshell.cn/articles/8031.html",
+		  "time" : "2012-08-15",
+		  "author" : "陈皓",
+		  "avatar" : "/static/images/avatar.png",
+		  "title" : "InfoQ的ArchSummit大会对我的采访",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/803.html"
+		  }
+		}
+	},
+	{
+		"id":802,
+		"content" : {
+		  "from" : "http://www.ux265.net",
+		  "url" : "http://www.google.com",
+		  "time" : "2012-08-15",
+		  "author" : "玉伯也叫射雕",
+		  "avatar" : "http://tp3.sinaimg.cn/1748374882/50/5611315601/1",
+		  "title" : "日常工作与新技术",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/802.html"
+		  }
+		}
+	},
+	{
+		"id":801,
+		"content" : {
+		  "from" : "http://www.ux265.net",
+		  "url" : "http://hi.baidu.com/lijing00333/item/1c28309d8b46c7d41e427118",
+		  "time" : "2012-08-09",
+		  "author" : "拔赤",
+		  "avatar" : "http://tp4.sinaimg.cn/1700082927/50/5611238573/1",
+		  "title" : "前端开发--十日谈",
+		  "ajaxcontent" : {
+				"ajaxFlag" : true,
+				"ajaxSource" : "/201208/801.html"
+		  }
+		}
+	}
+]
